544550da33d2cf692064014e4bfad6ec0652d1e3
Marc-Olivier <islandx424@gmail.com>
2020-08-14 20:25:26 -0400

    Simple ball bounce mechanic. Bug on the wall

---
+++ Assets/Scripts/BounceBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceBall : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector2 direction = new Vector2(-1, 0);
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var contactPoint = collision.contacts[0].point;
+        direction = (contactPoint - (Vector2)collision.transform.position).normalized;
+    }
+}
